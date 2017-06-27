e8cdf3935f793544e68cb7408fcb7460d9b1efec
David Asabina <vid@bina.me>
2017-06-28 01:58:34 +0200

    rails g controller Sessions
    `rails g controller Sessions login logout redirect index`

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def login
+  end
+
+  def logout
+  end
+
+  def redirect
+  end
+
+  def index
+  end
+end
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get sessions_login_url
+    assert_response :success
+  end
+
+  test "should get logout" do
+    get sessions_logout_url
+    assert_response :success
+  end
+
+  test "should get redirect" do
+    get sessions_redirect_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get sessions_index_url
+    assert_response :success
+  end
+
+end

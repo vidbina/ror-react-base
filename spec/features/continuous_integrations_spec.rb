cb9532ef25649da4fa4c7e26599f8d6575d00b7b
David Asabina <vid@bina.me>
2017-07-03 00:48:46 +0200

    a few passing and failing tests

---
+++ spec/features/continuous_integrations_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "ContinuousIntegrations", type: :feature do
+  describe 'comparison' do
+    it 'of booleans' do
+      expect(true).to be false
+    end
+
+    it 'of integers' do
+      expect(1).to be > 0
+      expect(0).to be < 1
+    end
+  end
+end
